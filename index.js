ba42a59adc6c493841627ff0d32e0851e58b3af5
Tmaddox85 <97774955+Tmaddox86@users.noreply.github.com>
2023-03-03 03:25:31 +0000

    added express.json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require ('dotenv').config()
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
